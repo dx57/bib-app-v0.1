6a88a8f48a37ad0882e7a689fea5b89ea06df931
MoThe3rd@googlemail.com <MoThe3rd@googlemail.com@65038801-d6ca-c78a-0b77-478bcf2a0754>
2015-02-11 00:44:55 +0000

    Basic GUI for Splash-Screen.

--- BiBApp/src/com/uni/bradford/bib/LoginActivity.java
+++ BiBApp/src/com/uni/bradford/bib/LoginActivity.java
@@ -1,39 +1,76 @@
 package com.uni.bradford.bib;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.TextView;
 
 public class LoginActivity extends Activity
 {
-
+	// GUI elements
+	private Button btnLogin;
+	private CheckBox cbRememberMe;
+	private TextView tvForgotId;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
+		
+		// Connect to GUI views
+		btnLogin = (Button)findViewById(R.id.btnLogin);
+		cbRememberMe = (CheckBox)findViewById(R.id.cbRememberMe);
+		tvForgotId = (TextView)findViewById(R.id.tvForgotID);
+		
+		// Add Listener
+		btnLogin.setOnClickListener(new OnBtnLoginClickListener());
+		cbRememberMe.setOnCheckedChangeListener(new OnCbRememberMeChangeListener());
+		tvForgotId.setOnClickListener(new OnTvForgotIdClickListener());
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu)
+	
+	private class OnBtnLoginClickListener implements OnClickListener
 	{
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.login, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item)
-	{
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings)
+		@Override
+		public void onClick(View v)
 		{
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
+			// TODO: Add behaviour
+			System.out.println("Button Login clicked");	
+			
+			// Change to overview activity 
+			Intent changeToOverview = new Intent(LoginActivity.this, OverviewActivity.class);
+			changeToOverview.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+			startActivity(changeToOverview);
+			
+			// Prevent user to go back to splash screen
+			LoginActivity.this.finish();
+		}	
 	}
+	
+	private class OnTvForgotIdClickListener implements OnClickListener
+	{
+		@Override
+		public void onClick(View v)
+		{
+			// TODO: Add behaviour
+			System.out.println("TextView Login clicked");			
+		}	
+	}
+	
+	private class OnCbRememberMeChangeListener implements OnCheckedChangeListener
+	{
+		@Override
+		public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
+		{
+			// TODO: Add behaviour
+			System.out.println("Checker RememberMe clicked state: " + isChecked);
+		}	
+	}
+
 }
