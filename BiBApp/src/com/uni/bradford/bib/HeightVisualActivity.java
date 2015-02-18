cdc0c49e29af9f86f50e7518ad368b37fbf64a66
MoThe3rd@googlemail.com <MoThe3rd@googlemail.com@65038801-d6ca-c78a-0b77-478bcf2a0754>
2015-02-18 02:18:41 +0000

    Add spinners.

--- BiBApp/src/com/uni/bradford/bib/HeightVisualActivity.java
+++ BiBApp/src/com/uni/bradford/bib/HeightVisualActivity.java
@@ -9,9 +9,14 @@ import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.Spinner;
 
 public class HeightVisualActivity extends Activity
 {
@@ -20,6 +25,8 @@ public class HeightVisualActivity extends Activity
 	private ImageView ivOwnChildBottomScale;
 	private ImageView ivAverageChildMiddleScale;
 	private ImageView ivAverageChildBottomScale;
+	private Spinner sSelectChild;
+	private Spinner sSelectCriterion;
 		
 	private SeekBar sbTimeLine;
 
@@ -38,12 +45,26 @@ public class HeightVisualActivity extends Activity
 		ivOwnChildBottomScale = (ImageView)findViewById(R.id.ivOwnChildBottomScale);
 		ivAverageChildMiddleScale = (ImageView)findViewById(R.id.ivAverageChildMiddleScale);
 		ivAverageChildBottomScale = (ImageView)findViewById(R.id.ivAverageChildBottomScale);
+		sSelectChild = (Spinner)findViewById(R.id.sSelectChild);
+		sSelectCriterion = (Spinner)findViewById(R.id.sSelectCriterion);
+		
+		// TODO: Just basic approach to get going.. not full functional
+		String[] childs = new String[] {"Child 2007", "Child 2009", "Child 2010"};
+		ArrayAdapter<String> adapterChilds = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, childs);
+		adapterChilds.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		sSelectChild.setAdapter(adapterChilds);
+		String[] criterion = new String[] {"Average", "Tallest", "Smallest"};
+		ArrayAdapter<String> adapterCriterion = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, criterion);
+		adapterCriterion.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		sSelectCriterion.setAdapter(adapterCriterion);
 		
 		sbTimeLine = (SeekBar)findViewById(R.id.sbTimeLine);
 		sbTimeLine.setMax(100);
 		
 		// Add listener
 		sbTimeLine.setOnSeekBarChangeListener(new OnSeekBarTimeLineChangeListener());
+		sSelectChild.setOnItemSelectedListener(new OnSpinnerSelectChildSelectedListener());
+		sSelectCriterion.setOnItemSelectedListener(new OnSpinnerSelectCriterionSelectedListener());
 	}
 	
 	private class OnSeekBarTimeLineChangeListener implements OnSeekBarChangeListener
@@ -74,6 +95,38 @@ public class HeightVisualActivity extends Activity
 
 		@Override
 		public void onStopTrackingTouch(SeekBar seekBar) { }
+	}
+	
+	private class OnSpinnerSelectChildSelectedListener implements OnItemSelectedListener
+	{
+		@Override
+		public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
+		{
+			// TODO: Add behaviour
+			System.out.println("Selected child: " + position);
+		}
+
+		@Override
+		public void onNothingSelected(AdapterView<?> parent)
+		{
+			// TODO: Select the first		
+		}
+	}
+	
+	private class OnSpinnerSelectCriterionSelectedListener implements OnItemSelectedListener
+	{
+		@Override
+		public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
+		{
+			// TODO: Add behaviour
+			System.out.println("Selected criteria: " + position);
+		}
+
+		@Override
+		public void onNothingSelected(AdapterView<?> parent)
+		{
+			// TODO: Select the first		
+		}
 	}
 	
 	@Override
