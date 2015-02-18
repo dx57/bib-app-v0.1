4f3ba1a4efaa0a5d52196c2eb51991f9cf1b7592
MoThe3rd@googlemail.com <MoThe3rd@googlemail.com@65038801-d6ca-c78a-0b77-478bcf2a0754>
2015-02-18 00:22:09 +0000

    Add action bar icon for share option.

--- BiBApp/src/com/uni/bradford/bib/OverviewActivity.java
+++ BiBApp/src/com/uni/bradford/bib/OverviewActivity.java
@@ -2,13 +2,6 @@ package com.uni.bradford.bib;
 
 import java.util.ArrayList;
 
-
-
-
-
-
-
-
 import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
@@ -16,8 +9,6 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.AdapterView;
