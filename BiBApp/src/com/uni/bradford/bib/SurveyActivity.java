1a494c32cbc6776ddc967968ebf9b2ce9b0ca4fd
MoThe3rd@googlemail.com <MoThe3rd@googlemail.com@65038801-d6ca-c78a-0b77-478bcf2a0754>
2015-02-20 04:12:43 +0000

    Add survey activity and change icon of activities.

---
+++ BiBApp/src/com/uni/bradford/bib/SurveyActivity.java
@@ -0,0 +1,34 @@
+package com.uni.bradford.bib;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebView;
+
+public class SurveyActivity extends Activity
+{
+	private WebView wvSurvey;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_survey);
+		
+		// Change ActionBar color and icon
+		ActionBar bar = getActionBar();
+		bar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#0171bd")));
+		bar.setIcon(R.drawable.ic_action_share);
+		
+		// Connect to GUI views
+		wvSurvey = (WebView)findViewById(R.id.wvSurvey);
+//		wvSurvey.getSettings().setJavaScriptEnabled(true);
+		
+		// TODO: Ask David how to connect to a survey
+		wvSurvey.loadUrl("https://www.surveymonkey.com/r/?sm=d9yyh03hx%2fRxh26ptsvay03MP0ZkErSidp5ni5TkqGw%3d");
+	}
+} 
