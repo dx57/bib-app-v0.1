778153d9dc219c631365a81c3328e1613fad0517
MoThe3rd <MoThe3rd@googlemail.com>
2015-03-02 19:08:42 +0000

    Implement survey behaviour.

--- BiBApp/src/com/uni/bradford/bib/SurveyActivity.java
+++ BiBApp/src/com/uni/bradford/bib/SurveyActivity.java
@@ -2,14 +2,24 @@ package com.uni.bradford.bib;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class SurveyActivity extends Activity
 {
+	// GUI
 	private WebView wvSurvey;
+	private ProgressBar pbLoadSurvey;
+	private TextView tvInfoToSurvey;
+	
+	// Logic
+	private static final String SURVEY_COMPLETE = "survey-thanks";
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
@@ -22,13 +32,51 @@ public class SurveyActivity extends Activity
 		bar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#0171bd")));
 		bar.setIcon(R.drawable.ic_survey_white);
 		
-		// Connect to GUI views
+		// Connect to GUI views and setup	
 		wvSurvey = (WebView)findViewById(R.id.wvSurvey);
+		pbLoadSurvey = (ProgressBar)findViewById(R.id.pbLoadSurvey);
+		tvInfoToSurvey = (TextView)findViewById(R.id.tvInfoToSurvey);
 		
-		// Must-Have for SurveyMonkey
+		// Must-Have for SurveyMonkey.. but might allow cross-side-scripting
 		wvSurvey.getSettings().setJavaScriptEnabled(true);
 		
-		// TODO: Ask David how to connect to a survey
-		wvSurvey.loadUrl("https://www.surveymonkey.com/r/?sm=d9yyh03hx%2fRxh26ptsvay03MP0ZkErSidp5ni5TkqGw%3d");
+		// TODO: Get survey via intent parameter to receive recent survey
+		// TODO: Use start activity for result to disable button if user took survey
+		// Use SurveyMonkey to deal with survey options 
+		wvSurvey.loadUrl("https://www.surveymonkey.com/s/HMP398J");
+		
+		// Add listener
+		wvSurvey.setWebViewClient(new SurveyWebViewClient());
+	}
+	
+	private class SurveyWebViewClient extends WebViewClient
+	{
+		public void onPageFinished(WebView view, String url) 
+		{
+			System.out.println("load " + url + " complete.");
+			
+			// Only if user starts the survey
+			if (!url.contains(SURVEY_COMPLETE))
+			{
+				// Hide progress bar and show loaded survey
+				tvInfoToSurvey.setVisibility(TextView.INVISIBLE);
+				pbLoadSurvey.setVisibility(ProgressBar.INVISIBLE);
+				wvSurvey.setVisibility(WebView.VISIBLE);
+			}
+		}
+		
+		public void onPageStarted(WebView view, String url, Bitmap favicon) 
+		{
+			System.out.println("started " + url);
+			
+			// Indicates the end of the survey
+			if (url.contains(SURVEY_COMPLETE))
+			{
+				// Thank user and hide unimportant views
+				wvSurvey.setVisibility(WebView.INVISIBLE);
+				tvInfoToSurvey.setText(R.string.survey_thanks);
+				tvInfoToSurvey.setVisibility(TextView.VISIBLE);
+			}
+	    }
 	}
 } 
