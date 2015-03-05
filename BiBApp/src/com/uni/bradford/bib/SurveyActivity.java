7c55d9a37458892ed9c3d7ade2a14b34479fd150
MoThe3rd@googlemail.com <MoThe3rd@googlemail.com@65038801-d6ca-c78a-0b77-478bcf2a0754>
2015-03-05 03:01:55 +0000

    Add comments and BraodcastReiver for network connection state changes.

--- BiBApp/src/com/uni/bradford/bib/SurveyActivity.java
+++ BiBApp/src/com/uni/bradford/bib/SurveyActivity.java
@@ -2,10 +2,15 @@ package com.uni.bradford.bib;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -22,6 +27,8 @@ public class SurveyActivity extends Activity
 	// Logic
 	private static final String SURVEY_COMPLETE = "survey-thanks";
 	private boolean tookSurvey;
+	private String surveyUrl;
+	private BroadcastReceiver networkStateBroadcastReceiver;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
@@ -31,6 +38,10 @@ public class SurveyActivity extends Activity
 		
 		// Init logic
 		tookSurvey = false;
+		networkStateBroadcastReceiver = new NetworkStateBroadcastReceiver();
+		IntentFilter intentFilter = new IntentFilter();
+		intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
+		registerReceiver(networkStateBroadcastReceiver, intentFilter);
 		
 		// Change ActionBar color and icon
 		ActionBar bar = getActionBar();
@@ -45,9 +56,9 @@ public class SurveyActivity extends Activity
 		// Must-Have for SurveyMonkey.. but might allow cross-side-scripting
 		wvSurvey.getSettings().setJavaScriptEnabled(true);
 		
-		// TODO: Use start activity for result to disable button if user took survey
 		// Use SurveyMonkey to deal with survey options 
-		wvSurvey.loadUrl(getIntent().getStringExtra(OverviewListViewAdapter.SURVEY_URL));
+		surveyUrl = getIntent().getStringExtra(OverviewListViewAdapter.SURVEY_URL);
+		wvSurvey.loadUrl(surveyUrl);
 		
 		// Add listener
 		wvSurvey.setWebViewClient(new SurveyWebViewClient());
@@ -55,8 +66,29 @@ public class SurveyActivity extends Activity
 		// TODO: Check for Internet connection and aleart user if no connection
 	}
 	
+	@Override
+	protected void onDestroy()
+	{
+		super.onDestroy();
+		
+		unregisterReceiver(networkStateBroadcastReceiver);
+	}
+	
 	private class SurveyWebViewClient extends WebViewClient
 	{
+		@Override
+	    public boolean shouldOverrideUrlLoading(WebView  view, String  url)
+		{
+			if (url.contains(surveyUrl) || url.contains(SURVEY_COMPLETE))
+			{
+				// User started or finished survey.. do react
+				return false;
+			}
+			
+			// User clicked an unexpected link.. do not react
+	        return true;
+	    }
+		
 		public void onPageFinished(WebView view, String url) 
 		{
 			System.out.println("load " + url + " complete.");
@@ -84,8 +116,33 @@ public class SurveyActivity extends Activity
 				tvInfoToSurvey.setVisibility(TextView.VISIBLE);
 				
 				tookSurvey = true;
-			}
+			}			
 	    }
+	}
+	
+	// TODO: Extract class to own file, because I will need it at least also for the login screen
+	public class NetworkStateBroadcastReceiver extends BroadcastReceiver  
+	{
+		@Override
+		public void onReceive(Context context, Intent intent)
+		{
+			ConnectivityManager connectivityManager = (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+			NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+			
+			if (networkInfo != null && networkInfo.isConnected())
+			{
+				
+				System.out.println("CONNECTED!!!!");
+			}
+			else
+			{
+				// TODO: Add Alert dialog here
+				
+				System.out.println("DIS-CONNECTED!!!!");
+			}
+			
+		}
+		
 	}
 	
 	@Override
