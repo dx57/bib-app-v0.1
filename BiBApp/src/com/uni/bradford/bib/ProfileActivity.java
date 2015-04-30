7a8bdcdefbaed06dedec19f606eb163b82834107
MoThe3rd@googlemail.com <MoThe3rd@googlemail.com@65038801-d6ca-c78a-0b77-478bcf2a0754>
2015-04-30 01:34:37 +0000

    Create ProfileActivity and content, implement to load data model

--- BiBApp/src/com/uni/bradford/bib/ProfileActivity.java
+++ BiBApp/src/com/uni/bradford/bib/ProfileActivity.java
@@ -1,16 +1,36 @@
 package com.uni.bradford.bib;
 
+import java.util.ArrayList;
+
 import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Spinner;
 
 public class ProfileActivity extends Activity
 {
-
+	// GUI
+	private Spinner sSelectChild;
+	private ListView lvProgram;
+	private ProgramListViewAdapter listAdapter;
+	
+	// Logic
+	private DataModel dataModel;
+	
+	// Model 
+	private ArrayList<ProgramEntry> programList;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
@@ -21,27 +41,142 @@ public class ProfileActivity extends Activity
 		ActionBar bar = getActionBar(); 
 		bar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#0171bd")));
 		bar.setIcon(R.drawable.ic_profile_white); 
+		
+		// Init logic
+		LoadDataModelFromFileAsyncTask loadLocalTask = new LoadDataModelFromFileAsyncTask();
+		loadLocalTask.execute();
+		
+		// Init local data-model
+		programList = new ArrayList<ProgramEntry>(); 
+				
+		// Connect to GUI views and setup	
+		sSelectChild = (Spinner)findViewById(R.id.sProfileSelectChild);
+		
+		// Setup program list
+		listAdapter = new ProgramListViewAdapter(programList, this);
+		lvProgram = (ListView)findViewById(R.id.lvOverview);
+		lvProgram.setDivider(null);
+		lvProgram.setAdapter(listAdapter);
+		
+		// Add listener
+		sSelectChild.setOnItemSelectedListener(new OnSpinnerSelectChildSelectedListener());
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu)
-	{
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.profile, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item)
-	{
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings)
+	
+	public void updateGui()
+	{		
+		String[] children = new String[dataModel.getMother().getChildCount()];
+		for (int i = 0; i < dataModel.getMother().getChildCount(); i++)
 		{
-			return true;
+			children[i] = dataModel.getMother().getChild(i).getYearOfBirth() + 
+					      "-" + dataModel.getMother().getChild(i).getMonthOfBirth();
 		}
-		return super.onOptionsItemSelected(item);
+		
+		ArrayAdapter<String> adapterChilds = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, children);
+		adapterChilds.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		sSelectChild.setAdapter(adapterChilds);
+		
+		changeProgramList(0);
+	}
+	
+	private class OnSpinnerSelectChildSelectedListener implements OnItemSelectedListener
+	{
+		@Override
+		public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
+		{
+			changeProgramList(position);
+		}
+
+		@Override
+		public void onNothingSelected(AdapterView<?> parent) { }
+	}
+	
+	private void changeProgramList(int position)
+	{
+		programList.clear();
+		
+		if (dataModel == null)
+		{
+			return;
+		}
+		
+		String[] programs = getResources().getStringArray(R.array.programs);
+		
+		if (1 >= dataModel.getMother().getChild(position).getEclipse())
+		{
+			programList.add(new ProgramEntry(true, programs[0], getResources().getString(R.string.hello_world)));
+		}
+		else
+		{
+			programList.add(new ProgramEntry(false, programs[0], getResources().getString(R.string.hello_world)));
+		}
+		
+		if (1 >= dataModel.getMother().getChild(position).getPrimaryCare())
+		{
+			programList.add(new ProgramEntry(true, programs[1], getResources().getString(R.string.hello_world)));
+		}
+		else
+		{
+			programList.add(new ProgramEntry(false, programs[1], getResources().getString(R.string.hello_world)));
+		}
+		
+		if (1 >= dataModel.getMother().getChild(position).getEducation())
+		{
+			programList.add(new ProgramEntry(true, programs[2], getResources().getString(R.string.hello_world)));
+		}
+		else
+		{
+			programList.add(new ProgramEntry(false, programs[2], getResources().getString(R.string.hello_world)));
+		}
+		
+		if (1 >= dataModel.getMother().getChild(position).getBib1000())
+		{
+			programList.add(new ProgramEntry(true, programs[3], getResources().getString(R.string.hello_world)));
+		}
+		else
+		{
+			programList.add(new ProgramEntry(false, programs[3], getResources().getString(R.string.hello_world)));
+		}
+		
+		if (1 >= dataModel.getMother().getChild(position).getMeDall())
+		{
+			programList.add(new ProgramEntry(true, programs[4], getResources().getString(R.string.hello_world)));
+		}
+		else
+		{
+			programList.add(new ProgramEntry(false, programs[4], getResources().getString(R.string.hello_world)));
+		}
+		
+		if (1 >= dataModel.getMother().getChild(position).getAll_in())
+		{
+			programList.add(new ProgramEntry(true, programs[5], getResources().getString(R.string.hello_world)));
+		}
+		else
+		{
+			programList.add(new ProgramEntry(false, programs[5], getResources().getString(R.string.hello_world)));
+		}
+	}
+	
+	private class LoadDataModelFromFileAsyncTask extends AsyncTask<Void, Void, Void>
+	{
+		@Override
+		protected Void doInBackground(Void... params)
+		{		
+			dataModel = DataModel.loadFromFile(ProfileActivity.this.getFilesDir());
+						
+			return null; 
+		}
+		
+		@Override
+		protected void onPostExecute(Void result)
+		{	
+			if (dataModel != null)
+			{
+				// Update GUI
+				updateGui();
+				
+//				Toast toast = Toast.makeText(HeightDiagramActivity.this, "..loaded from file", Toast.LENGTH_SHORT);
+//				toast.show();
+			}
+		}
 	}
 }
